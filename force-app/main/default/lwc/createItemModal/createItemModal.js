d488534d19c672aeec2e0e270d47a50a6acdd2f6
Anton Shantsov <shantsov@gmail.com>
2025-08-31 19:12:24 +0300

    Unsplash API
    added opportunity to bind photos to product also fixed label error

--- force-app/main/default/lwc/createItemModal/createItemModal.js
+++ force-app/main/default/lwc/createItemModal/createItemModal.js
@@ -1,4 +1,9 @@
 import { LightningElement, track } from 'lwc';
+import getImageUrl from '@salesforce/apex/UnsplashService.getImageUrl';
+import { updateRecord } from 'lightning/uiRecordApi';
+import ID_FIELD from '@salesforce/schema/Item__c.Id';
+import IMAGE_FIELD from '@salesforce/schema/Item__c.Image__c';
+
 export default class CreateItemModal extends LightningElement {
     @track isOpen = false;
 
@@ -10,7 +15,24 @@ export default class CreateItemModal extends LightningElement {
         this.isOpen = false;
     }
 
-    handleSuccess() {
+    handleSuccess(event) {
+        const itemId = event.detail.id;
+        const itemName = event.detail.fields.Name.value;
+
+        // обращаемся к Unsplash
+        getImageUrl({ query: itemName })
+            .then(url => {
+                if (url) {
+                    const fields = {};
+                    fields[ID_FIELD.fieldApiName] = itemId;
+                    fields[IMAGE_FIELD.fieldApiName] = url;
+
+                    const recordInput = { fields };
+                    updateRecord(recordInput);
+                }
+            })
+            .catch(err => console.error(err));
+
         this.isOpen = false;
     }
 }
